a22ed09daa877c773a1df78f01f8ce0524659946
Drashti106 <dgdoshi.2003@gmail.com>
2023-03-29 18:29:16 +0530

    make class static and public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,12 +7,12 @@ public class Main {
       b.meth();
   }
     public static class A{
-        String meth(){
+        public String meth(){
             return "Invoking method from class A";
         }
     }
     public static class B extends A{
-        String meth(){
+        public String meth(){
             return "Method is overridden in Extendend class B";
         }
     }
